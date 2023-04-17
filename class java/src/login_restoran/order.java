4f2c53681a72f80e469c906271045d8360dab289
madejauhari <madejauhari2004@gmail.com>
2023-04-17 19:31:38 +0800

    membuat class order

---
+++ class java/src/login_restoran/order.java
@@ -0,0 +1,11 @@
+package login_restoran;
+
+public class order {
+
+    static void MenuOrder(){
+        System.out.println("======== MENU ORDER =======");
+        System.out.println("|1. Lihat Restoran         ");
+        System.out.println("|2. Logout                 ");
+        System.out.println("===========================");
+    }
+}
